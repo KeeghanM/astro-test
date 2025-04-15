8ed6f091bd5f24e3bb7877cc05f1df17230341ee
Keeghan McGarry <keeghan_m@live.com>
2025-04-15 10:53:02 +0100

    Blog

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,9 @@ import { defineConfig } from 'astro/config'
 
 import cloudflare from '@astrojs/cloudflare'
 
-import react from '@astrojs/react';
+import react from '@astrojs/react'
+
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0'
 
 // https://astro.build/config
 export default defineConfig({
