ef47ee8c4e902d96aa7c34c9fd2ff6941708bae2
Keeghan McGarry <keeghan_m@live.com>
2025-04-22 10:32:18 +0100

    Clean start

---
+++ src/components/navbar/navbar.tsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+  return (
+    <nav className="navbar">
+      <ul className="navbar__links">
+        <li className="navbar__link">Home</li>
+        <li className="navbar__link">About</li>
+        <li className="navbar__link">Contact</li>
+      </ul>
+    </nav>
+  );
+}
