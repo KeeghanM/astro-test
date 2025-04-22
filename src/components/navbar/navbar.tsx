a079952b6f8155614cf9ec7d95881c98d6c5668c
Keeghan McGarry <keeghan_m@live.com>
2025-04-22 10:41:49 +0100

    sdtyles

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,10 +1,33 @@
+import { useState } from 'react';
+
 export default function Navbar() {
+    const links = [
+        {
+            title: "Home",
+            URL: "/"
+        },
+        {
+            title: "About",
+            URL: "/about"
+        },
+        {
+            title: "Contact",
+            URL: "/contact"
+        }
+    ]
+
+    const [currentLink, setCurrentLink] = useState(links[0].title);
   return (
     <nav className="navbar">
       <ul className="navbar__links">
-        <li className="navbar__link">Home</li>
-        <li className="navbar__link">About</li>
-        <li className="navbar__link">Contact</li>
+        {links.map((link, index) => (
+          <li key={index} className="navbar__link" onClick={() => setCurrentLink(link.title)}>
+            <a style={{
+                color: currentLink === link.title ? "blue" : "black",
+                textDecoration: currentLink === link.title ? "underline" : "none"
+            }} className="navbar__link-text" >{link.title}</a>
+            </li>
+        ))}
       </ul>
     </nav>
   );
